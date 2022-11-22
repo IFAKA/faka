6c2941dbd0135d12a3ba62c9d4a033ee2b7283d0
IFAKA <me@faka.dev>
2022-11-22 13:30:20 -0300

    UIUX improvement

--- src/utils/formatApi/formatApi.ts
+++ src/utils/formatApi/formatApi.ts
@@ -1,13 +1,13 @@
 import { IProject, Repo } from "@/models"
 import { sentenceCase } from "../sentenceCase"
 
-const formatApi = (apiRes:Repo[]): IProject[] =>
+const formatApi = (apiRes: Repo[]): IProject[] =>
   apiRes?.map((repo) => ({
     id: repo.id,
     href: repo.homepage ?? "",
     name: sentenceCase(repo.name).replace(/-/g, " "),
     desc: repo.description ?? "",
-    tags: repo.topics.map((topic: string) => sentenceCase(topic)),
+    tags: repo.topics,
   }))
 
 export default formatApi
